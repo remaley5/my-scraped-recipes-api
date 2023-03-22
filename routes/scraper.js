86ec68f814551baefe7d1cdc370970dc0f2238f8
remaley5 <swillson@alumni.risd.edu>
2023-03-22 14:56:51 -0700

    post route working, url param from frontend

--- routes/scraper.js
+++ routes/scraper.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res, next) {
     puppeteer.launch().then(async function(browser) {
         const page = await browser.newPage();
         await page.goto("https://www.allrecipes.com/recipe/20144/banana-banana-bread");
+        // await page.goto(url);
         const data = await page.evaluate(() => {
             const ingredients = document.querySelectorAll('.mntl-structured-ingredients__list li');
             const array = [];
@@ -26,4 +27,33 @@ router.get('/', function(req, res, next) {
   //const browser = await puppeteer.launch( { headless: false } )
 
 });
+
+router.post('/useurl', function(req, res, next) {
+    puppeteer.launch().then(async function(browser) {
+        const {url} = req.body;
+        console.log('url', url);
+        const page = await browser.newPage();
+        await page.goto(url);
+        // await page.goto(url);
+        const data = await page.evaluate(() => {
+            const ingredients = document.querySelectorAll('.mntl-structured-ingredients__list li');
+            const array = [];
+            // Enter JavaScript to run on the page here!
+            for(i=0; i<ingredients.length; i++) {
+                array.push({
+                    quantity: ingredients[i].querySelector('[data-ingredient-quantity]').innerText,
+                    unit: ingredients[i].querySelector('[data-ingredient-unit]').innerText, 
+                    name: ingredients[i].querySelector('[data-ingredient-name]').innerText
+                })
+            }
+            return array;
+        })
+        await browser.close();
+        res.json({data})
+    })
+  //const browser = await puppeteer.launch( { headless: false } )
+
+});
+
+
 module.exports = router;
